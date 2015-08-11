96fd8edec94e7d51355b3a8a80b0868ef752329d
Nicholas Kim <nicholaskim94@gmail.com>
2015-08-11 16:27:18 +0900

    Change routes.rb to redirect to index#timer when closed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  match ":controller(/:action(/:id))", :via => [:post, :get]
+	if Time.now.hour < 22 || Time.now.hour <5 
+		root :to => "index#timer" 
+	else
+		root :to => "index#intro"
+	end
+	match ":controller(/:action(/:id))", :via => [:post, :get]
 end
