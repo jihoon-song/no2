c9c5b9433eac5bb08b8bae63b84f8fbea1e370fd
이준희 <junhee428@naver.com>
2015-08-13 13:50:28 +0900

    Hotfix on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	if Time.now.hour < 22 || Time.now.hour <5 
+	if Time.zone.now.hour < 22 || Time.zone.now.hour > 5 
 		root :to => "index#timer" 
 	else
 		root :to => "index#intro"
