e1adde38a82e8997ec2039a3db0aec1da3faabbd
Nicholas Kim <nicholaskim94@gmail.com>
2015-08-16 20:32:46 +0900

    Add new, write action in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
 	def show
-		@post = Post.find(params[:id])
+		@posts = Post.find(params[:id])
+	end
+
+	def new
+		@user_id = current_user.id
+	end
+
+	def write
+		@post = Post.new
+		@post.user_id = params[:id]
+		@post.title = params[:title]
+		@post.content = params[:content]
+		@post.save
 	end
 end
